fb981e487d3643e3e630a3455b4b98feeea22788
nicole <nicolerodriguezcab@gmail.com>
2024-12-19 18:40:06 -0500

    adde tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: [
+		"./src/**/*.{astro,html,js,jsx,ts,tsx}", // Add this to scan for Tailwind CSS classes
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
